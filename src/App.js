8b2c3456249b584cce8429ee6a6b1fd551451cfc
Alex Myers <amyers3000@Alexs-MBP.attlocal.net>
2022-06-12 08:31:52 -0500

    inital commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  let [counter, setCounter] = useState(1)
+  window.setCounter = setCounter
+  return (
+    <div >
+      <h1>{counter}</h1>
+      <h1>{counter}</h1>
+    </div>
+  );
+}
+
+export default App;
